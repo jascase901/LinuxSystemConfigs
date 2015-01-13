50d994442e339bc75b88af9ef9e4d0743a66458c
Jason Hilton <jasface901@gmail.com>
2015-01-12 23:36:01 -0800

    added tmux.conf

--- BoostStrap.sh
+++ BoostStrap.sh
@@ -5,4 +5,12 @@ then
    cp $HOME/.vimrc $HOME/.vimrc.back
 fi
 cp .vimrc $HOME/.vimrc
+
 echo "Vundle Install  complete open vim and run :PluginInstall to download rest of plugins from git"
+
+if [ -f $HOME/.tmux.conf ];
+then 
+   echo "File ~/.tmux.conf exists copying back to $HOME/.tmux.conf.backup"
+   cp $HOME/.tmux.conf $HOME/.tmux.conf.back
+fi
+cp .tmux.conf $HOME/.tmux.conf
